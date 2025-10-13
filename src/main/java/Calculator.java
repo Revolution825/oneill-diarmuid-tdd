3cc60f279771287fcc8179865bc8568a7ca0d1ce
Revolution825 <diarmuidoneill2003@gmail.com>
2025-10-13 10:18:26 +0100

    divideByZero Passing

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -14,6 +14,9 @@ public class Calculator {
     }
 
     public int divide(int a, int b) {
+        if (a == 0 || b == 0) {
+            throw new IllegalArgumentException("Cannot divide by 0");
+        }
         return b / a;
     }
 }
