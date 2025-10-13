6c62ae9ed03f155255ece84f073444bdd236be65
Revolution825 <diarmuidoneill2003@gmail.com>
2025-10-13 09:47:59 +0100

    addTest Failing

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,13 @@
 package test.java;
 
+import main.java.Calculator;
+
+import org.junit.Test;
+
 class CalculatorTest {
-
-
+    @Test
+    void addTest(int a, int b) {
+        Calculator c = new Calculator();
+        assertEquals(5, c.add(2, 3));
+    }
 }
